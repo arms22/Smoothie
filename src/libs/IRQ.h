cd44a4c004ce6411318f2e4a2deafe9c570652d8
arms22 <arms22@gmail.com>
2015-03-29 21:06:19 +0900

    Revert "fix compile error"
    This reverts commit 7b3de1d149048f1e171ca9ae3e530185b57cc7d1.

--- src/libs/IRQ.h
+++ src/libs/IRQ.h
@@ -1,16 +1,16 @@
 #ifndef IRQ_H
 #define IRQ_H
 
-#include "sLPC17xx.h"
+#include <cmsis.h>
 
-static __INLINE uint32_t disableIRQ(void)
+__STATIC_INLINE uint32_t disableIRQ(void)
 {
     uint32_t primask = __get_PRIMASK();
     __disable_irq();
     return primask;
 }
 
-static __INLINE void restoreIRQ(uint32_t oldprimask)
+__STATIC_INLINE void restoreIRQ(uint32_t oldprimask)
 {
     __set_PRIMASK(oldprimask);
 }
